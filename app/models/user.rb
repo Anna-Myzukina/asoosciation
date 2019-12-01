4f074f8daaf25fdef8739b596e3ddc90db55d511
Anna Myzukina <anna.muzykina83@gmail.com>
2019-12-01 22:37:29 +0200

    Initialise repo and add main folders/files for app

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
+
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event
+
+  has_many :sent_invitations, class_name: 'Invitation', foreign_key: 'inviter_id'
+  has_many :received_invitations, class_name: 'Invitation', foreign_key: 'invited_id'
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+                    def User.digest(string)
+                      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                                    BCrypt::Engine.cost
+                      BCrypt::Password.create(string, cost: cost)
+                    end
+end
